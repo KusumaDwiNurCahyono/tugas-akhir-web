4824383eedf3e93049dd3ac78216f16481a8cc83
KusumaDwiNurCahyono <lamb_of_blind@yahoo.com>
2015-01-22 14:28:03 +0700

    Revert "tugas akhir"
    This reverts commit 9ea27ac4aef3266fd5300b1e8c5a5d8aadd786a6.

--- sibos/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$lokal="localhost";
-	$user="root";
-	$password="";
-	$db="jobhouse";
-
-	$koneksi=mysql_connect($lokal, $user, $password);
-	mysql_select_db($db, $koneksi) or die ("Koneksi gagal bossss");
-?>
